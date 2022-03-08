ee0edb63f4b1a3c4079b8afef00ee3da387117de
AckonSamuel <ackons433@gmail.com>
2022-03-08 13:20:37 +0000

    Improve styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './style.css';
 
 const li = document.querySelectorAll('li');
-const liArr =  Array.from(li);
+const liArr = Array.from(li);
 
-for(let i = 0; i < liArr.length; i++) {
-  if(i % 2 !== 0) 
- { liArr[i].style.background = '#aaa';}
+for (let i = 0; i < liArr.length; i += 1) {
+  if (i % 2 !== 0) { liArr[i].style.background = '#aaa'; }
 }
