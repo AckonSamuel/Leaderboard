e1f3dfe9a457bf5579108f27ab9273392ffc6609
AckonSamuel <ackons433@gmail.com>
2022-03-08 13:14:43 +0000

    Modify styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const li = document.querySelectorAll('li');
+const liArr =  Array.from(li);
+
+for(let i = 0; i < liArr.length; i++) {
+  if(i % 2 !== 0) 
+ { liArr[i].style.background = '#aaa';}
+}
