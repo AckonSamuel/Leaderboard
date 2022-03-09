79ccec353b8a26a4a0a8aa324b41fd9348ad2d73
AckonSamuel <ackons433@gmail.com>
2022-03-09 10:07:12 +0000

    Add function for game initialization

---
+++ src/modules/initialize_game.js
@@ -0,0 +1,13 @@
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const initialize_game = async (game_name) => {
+    const response = await fetch(`${url}`, {
+        method: 'POST',
+        body: JSON.stringify({name: game_name }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+    });
+    return response.json();
+};
+
+export { url, create_game };
