283587fe422b9a16c82e2dd01d1991e42bca35ac
AckonSamuel <ackons433@gmail.com>
2022-03-09 23:06:43 +0000

    Work on form chidren

--- src/index.js
+++ src/index.js
@@ -3,28 +3,21 @@ import add_score from './modules/add_score.js';
 import { initialize_game } from './modules/initialize_game.js';
 import get_result from './modules/retrieve_data.js';
 
-
-const li = document.querySelectorAll('li');
-const liArr = Array.from(li);
-
-for (let i = 0; i < liArr.length; i += 1) {
-  if (i % 2 !== 0) { liArr[i].style.background = '#aaa'; }
-};
-
 const user_name = document.getElementById('name');
 const user_score = document.getElementById('score');
-const form = document.querySelector('form');
+const refresh = document.querySelector('.refresh');
+const submit = document.querySelector('#submit');
 initialize_game('God of War');
-form.addEventListener('submit', () => {
-  add_score(user_name.value, user_score.value);
-  form.reset();
+
+submit.addEventListener('click', () => {
+  if( user_name.value != '' && user_score.value != ''){
+  add_score(user_name.value, user_score.value);}
+  console.log("worked");
 });
 
-const refresh = document.querySelector('.refresh');
  
 refresh.addEventListener('click', () => {
   window.location.reload();
-  get_result();
 })
 
 window.onload = () => get_result();
